b413cd5558cf38aa6ee2959683e63c66ef53a926
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2014-02-18 20:03:02 +0000

    Remove unused code

--- graphics.py
+++ graphics.py
@@ -2,7 +2,6 @@ from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.graphics import Color, Ellipse, Line, Rectangle, Mesh, Triangle
-from pyshull.earclipping import EarClipping
 import pickle, random, slippy, math
 
 gKeepProblemPolygons = False
@@ -30,40 +29,6 @@ def DrawLine(obj, width, DrawCallback, projObjs, tileCode, tileZoom, dash_length
 		li.dash_length = 10.
 		li.dash_offset = 10.
 	DrawCallback(li)
-
-def DrawPoly(obj, width, DrawCallback, projObjs, tileCode, tileZoom):
-
-	vertices = []
-	for node in obj:
-		nodePos = node[1]
-		if nodePos is None: continue #Missing node
-		x, y = Proj(nodePos[0], nodePos[1], projObjs)
-		vertices.append((x, y))
-
-	try:
-		vertices2, triangles = EarClipping(vertices, [])
-	except Exception as err:
-		print err
-		if gKeepProblemPolygons:
-			randFilename = "polyerr{0}.dat".format(random.randint(0,1000000))
-			pickle.dump((vertices, []), open(randFilename, "wb"))
-			print "Saved err polygon to", randFilename
-		return
-
-	#print triangles
-
-	for tri in triangles:
-		for ptNum in tri:
-			if ptNum < 0 or ptNum >= len(vertices2):
-				raise Exception("Out of bounds vertex index")
-
-	for tri in triangles:
-		triPos = []
-		for p in tri:
-			triPos.extend(list(vertices2[p]))
-
-		poly = Triangle(points = triPos)
-		DrawCallback(poly)
 
 def DrawTriPoly(obj, width, DrawCallback, projObjs, tileCode, tileZoom):
 
